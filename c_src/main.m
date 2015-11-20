5fe3d451b1f273a350b55fd550b34878b84c94a0
lanou3g <lanou3g@lanou3gdemac-mini-4.local>
2015-11-20 19:41:49 +0800

    整理字符串指针

--- c_src/main.m
+++ c_src/main.m
@@ -13,6 +13,29 @@
 
 void fun_1()
 {
+    //Created by lanou3g on 15/11/20.讲义
+    //1.定义二维数组,存储3个字符串
+    //char str[3][50]={"iOS","iPad","iMac"};
+    
+    //str本质上是二维数组
+    //str存储的内容为字符数组 (字符串)
+    
+    //str[0] 访问第一个字符数组,相当于第一个字符数组的地址 (首地址)
+    //str[1] 访问第二个字符数组 ,相当于第二个字符数组的地址
+    //str[2] 访问第三个字符数组, 相当于第三个字符数组的地址
+    //printf("%s\n",str[1]);//iPad
+    //printf("%s\n",*(str+1));//iPad
+    
+    
+     //2.指针数组,此时str本质上是一维数组，str中存储的是字符指针
+     //char *str[3]={"iOS","iPad","iMac"};
+     //str[0]是第一个字符指针 指向常量区字符串"ios"
+     //str[1]是第二个字符指针 指向常量区字符串"ipad"
+     //printf("%s\n",str[0]);//iOS
+     //str[0]="iPhone";//修改指向，可以
+    
+    
+    
     /* //ok
      char b2[]="hello";
      printf("%s\n",b2);
@@ -21,87 +44,53 @@ void fun_1()
      p=b;
      printf("%s\n",p);
      */
+    
     /* //ok
-     char *p2="123abc";//指针变量指向字符串常量的第一个字符。c语言只有字符变量，没有字符串变量
+     char *p2="123abc";//指针变量指向内存中字符串常量的第一个字符。c语言只有字符变量，没有字符串变量
      printf("%s\n",p2);
      printf("%s\n",strchr(p2, '3'));
      */
-    /* ?
-     char *p1=say_hello1();
-     printf("%s\n",p1);
+    
+    /*
+     int i[]={12,58,555,19};
+     int k[][3]={{15,78,55},{74,694,1},{88,5,98}};
+     char cc[]={"1122abc"};
+     char strings[][10] = {"iPhone" , "Android" , "win8" ,"123456789"};
+     char t=strings[2][3];
+     char *tt=strings[1];//字符串"Android"的地址
+     printf("%s",tt);
      */
-    //int k=echo_int();
-    //printf("%d\n",k);
-    //char *a=say_hello();
-    //say_hello1();
+    
+    /* ok
+     int a=12;
+     int *p=&a;
+     char *s[3] = { "iOS", "Android", "Win8"};//指针数组
+     char *t1=s[2];
+     char **t2=s+2;
+     
+     int k1=strcmp(s[1], s[2]);
+     k1=strcmp(s[2], s[1]);
+     printf("%s\n",t1);
+     printf("%s\n",*t2);
+     */
     
 }
-void foo_3(int *p1,int *p2)
-{
-    int temp=*p1;
-    *p1=*p2;
-    *p2=temp;
-}
-void fun_3()
-{
-    int a=89;
-    int b=97;
-    int *pointer_1=&a;
-    int *pointer_2=&b;
-    foo_3(pointer_1, pointer_2);
-    printf("%d,%d",a,b);
-}
-
-void fun_4()
-{
-    int a[3];
-    for (int i=0; i<3; i++) {
-        scanf("%d",a+i);
-    }
-    for (int i=0; i<3; i++) {
-        printf("结果%d",*(a+i));
-    }
-}
-
-void foo_5(int *p,int n)
-{
-    for (int i=0; i<n; i++)
-    {
-        for (int j=i+1; j<n; j++) {
-            if(*(p+j)>*(p+i))
-            {
-                int temp=*(p+j);
-                *(p+j)=*(p+i);
-                *(p+i)=temp;
-            }
-        }
-    }
-}
-void fun_5()
-{
-    int a[4]={12,45,3,7};
-    foo_5(a, 4);
-    
-    for (int i=0; i<4; i++) {
-        printf("排序结果%d\n",*(a+i));
-    }
-}
 
 
-void fun_6()
+void fun_2()
 {
     /*二维数组
      int a[][4]={{50,51,52,55},{70,71,72,73},{77,99,88,67}};
      int *p=a+2;//p指向77,99,88,67的一维数组的首地址
      for (int i=0; i<4; i++) {
-     printf("%d\n",*(p+i));//轮流打出77,99,88,67
+        printf("%d\n",*(p+i));//轮流打出77,99,88,67
      }
      */
     /*
      char b[][4]={"1234","abcd","wxyz"};
      char *p=b[1];
      for (int i=0; i<4; i++) {
-     printf("%c\n",p[i]);
+        printf("%c\n",p[i]);
      }
      */
     
@@ -234,66 +223,54 @@ void point_char_array2_foo()//二维字符串数组
     printf("%s,%s\n",*p,*(p+1));
 }
 
+void printCharArray(char **p,int n)//传入二维字符串数组的首地址,打印所有字符串
+{
+    for (int i=0; i<n; i++)
+    {
+        printf("%s\n",*(p+i));
+    }
+}
+void printOneCharArray(char *p)//只打印一个字符串
+{
+    printf("%s\n",p);
+    
+}
 void point_foo()
 {
-    //Created by lanou3g on 15/11/20.讲义
-    //1.定义二维数组,存储3个字符串
-    //char str[3][50]={"iOS","iPad","iMac"};
+    //point_int_array_foo()
+    //point_char_array2_foo();
     
-    //str本质上是二维数组
-    //str存储的内容为字符数组 (字符串)
+    //总结一般操作字符串数据的方法
+    //一、使用指针数组
+    //1.定义
+    char *charArray[3]={"123","abc","iOS"};
+    //2.借助**charArray输出所有字符串
+    printCharArray(charArray, 3);
+    //或者charArray[i],某一个字符串地址
+    for (int i=0; i<3; i++)
+    {
+        printOneCharArray(charArray[i]);//charArray[i]可写为*(charArray+i)
+    }
     
-    //str[0] 访问第一个字符数组,相当于第一个字符数组的地址 (首地址)
-    //str[1] 访问第二个字符数组 ,相当于第二个字符数组的地址
-    //str[2] 访问第三个字符数组, 相当于第三个字符数组的地址
-    //printf("%s\n",str[1]);//iPad
-    //printf("%s\n",*(str+1));//iPad
-    
-    /*
-     //2.指针数组,此时str本质上是一维数组，str中存储的是字符指针
-     char *str[3]={"iOS","iPad","iMac"};
-     //str[0]是第一个字符指针 指向常量区字符串"ios"
-     //str[1]是第二个字符指针 指向常量区字符串"ipad"
-     printf("%s\n",str[0]);//iOS
-     str[0]="iPhone";//修改指向，可以
-     */
+    //二、使用二维字符串数组
+    //1.定义
+    char str[3][50]={"java","C","Object-c"};
+    //2.charArray[i]指向某一个字符串地址
+    for (int i=0; i<3; i++)
+    {
+        printOneCharArray(str[i]);//str[i]可写为*(str+i)
+    }
     
     
-    
-    point_char_array2_foo();
 }
 
 
 int main(int argc, const char * argv[])
 {
-    //char b[]="1234567";
-    //int k0=strlen(b);
-    //printf("%d\n",k0);
-    //maopao_order();
-    /*
-     int i[]={12,58,555,19};
-     int k[][3]={{15,78,55},{74,694,1},{88,5,98}};
-     char cc[]={"1122abc"};
-     char strings[][10] = {"iPhone" , "Android" , "win8" ,"123456789"};
-     char t=strings[2][3];
-     char *tt=strings[1];//字符串"Android"的地址
-     printf("%s",tt);
-     */
-    /* ok
-     int a=12;
-     int *p=&a;
-     char *s[3] = { "iOS", "Android", "Win8"};//指针数组
-     char *t1=s[2];
-     char **t2=s+2;
-     
-     
-     int k1=strcmp(s[1], s[2]);
-     k1=strcmp(s[2], s[1]);
-     printf("%s\n",t1);
-     printf("%s\n",*t2);
-     */
-    
     //main_paixu();//两种排序方法及运行时间
+    
+    
+    
     point_foo();
     return 0;
 }
